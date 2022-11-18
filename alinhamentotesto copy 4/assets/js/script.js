831a59e0eef233de33045f3e733b2e0ae6e2323b
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2022-11-18 12:53:37 -0300

    feat

---
+++ alinhamentotesto copy 4/assets/js/script.js
@@ -0,0 +1,10 @@
+// const hed = document.querySelector('#hed');
+
+function clickMenu(){
+  if(itens.style.display=='block'){
+    itens.style.visibility= 'hidden';
+    hed.style.height='20vh';
+  }else{
+    itens.style.visibility= 'visible';
+  }
+}
