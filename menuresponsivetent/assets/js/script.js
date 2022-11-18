e3c3cd8c3abd8101d85547d8d58efbd31ad82dcb
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2022-11-18 18:35:38 -0300

    feat

--- menuresponsivetent/assets/js/script.js
+++ menuresponsivetent/assets/js/script.js
@@ -1,21 +1,7 @@
-const btnMobile = document.querySelector('#btn-mobile');
+const btn = document.getElementById('btn-menu')
 
+btn.addEventListener('click',toggleMenu)
 function toggleMenu(event){
-  if(event.type === 'touchstart') event.preventDefault()
-  const nav = document.getElementById('nav');
-  // nav.classList.add('active') pra adicionar ou remove para remover
-  nav.classList.toggle('active')
-  const active = nav.classList.contains('active')
-  //toglle adiciona class caso não tenha e remove caso tenha 
-  event.currentTarget.setAttribute('aria-expanded', active)
-  if(active){
-    event.currentTarget.setAttribute('aria-label','fechar Menu');
-  }else{
-    event.currentTarget.setAttribute('aria-label','Abrir Menu');
-  }
+  const menu = document.getElementById('menu')
+  menu.classList.toggle('active')
 }
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-// mobile emula um evento de tuch e demora 300 ms para fazelo 
-//ao clicar vai acionar a função 
